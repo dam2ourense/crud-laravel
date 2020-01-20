590d2e004006de9730a486ecaaf22ae90ef0a749
luis <luis@soy.yo>
2020-01-20 12:42:36 +0100

    creación registro y mensajes de ok o validacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.main')
 @section('content')
 <h1> Página HOME </h1>
+
+@if (session('successMsg'))
+
+<div class="alert alert-success" role="alert">
+        {{session('successMsg')}}
+</div>
+
+@endif
+
 @endsection
