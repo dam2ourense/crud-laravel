f00001301ba8057a6919bc2827da9de6dad84229
luis <luis@soy.yo>
2020-01-22 11:22:52 +0100

    operaciones básicas completadas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,22 @@
       <td>{{$estudiante->telefono}}</td>
       <td>
       <a class="btn btn-raised btn-primary btn-sm" href="{{route('edit',$estudiante->id)}}"> <i class="fas fa-edit"></i></a>
-      <a class="btn btn-raised btn-danger btn-sm" href="{{route('delete',$estudiante->id)}}"> <i class="fas fa-trash-alt"></i></a>
+      <form method="POST" id="delete-form" action="{{route('delete',$estudiante->id)}}" style="display: none;" >
+        {{csrf_field()}}
+        {{method_field('delete')}}
+      </form>
+      <button onclick="if (confirm('Estas seguro de borrar?')){
+        document.getElementById('delete-form').submit();  
+        }else{
+            event.preventDefault();
+        }      
+      "      
+      class="btn btn-raised btn-danger btn-sm" > <i class="fas fa-trash-alt"></i>
+      
+      
+      </button>
+      
+      <!-- <a class="btn btn-raised btn-danger btn-sm" href="{{route('delete',$estudiante->id)}}"> <i class="fas fa-trash-alt"></i></a> -->
       </td>
     </tr> 
   @endforeach
